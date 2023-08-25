4d728e5213713e52863b73f49a7961d1f0a027a0
Gabriel Chulim <gabriel.chulim@revenatium.com>
2023-08-25 14:29:13 -0500

    Section 9: Mockito [#111 Returning values from Mockito Mocks]

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,6 +24,18 @@ class SpecialitySDJpaServiceTest {
     @Test
     void delete() {
         service.delete(new Speciality());
+    }
+
+    @Test
+    void findById() {
+        Speciality speciality = new Speciality();
+
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1L);
+
+        assertThat(foundSpeciality).isNotNull();
+        verify(specialtyRepository).findById(1L);
     }
 
     @Test
